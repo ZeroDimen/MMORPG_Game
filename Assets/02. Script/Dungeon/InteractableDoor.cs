eeee40527b47b29d18d9579fb63a0e0cf08106f0
ZeroDimen <rlaehdrjs951@naver.com>
2026-09-01 20:26:51 +0900

    Add Dungeon OverheadDoor
    2026.09.01
    room 4 문 추가

--- Assets/02. Script/Dungeon/InteractableDoor.cs
+++ Assets/02. Script/Dungeon/InteractableDoor.cs
@@ -1,12 +1,22 @@
 using System.Collections;
+using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
 public class InteractableDoor : MonoBehaviour
 {
+    public enum DoorMode
+    {
+        SlidingDoor,
+        OverheadDoor
+    }
+
+    [SerializeField] private DoorMode doorMode;
     [SerializeField] private float openAngle = 90f;
     [SerializeField] private float rotateSpeed = 3f;
-
+    [SerializeField] private float closeY = 0f;
+    [SerializeField] private float openY = 3.5f;
+    
     private bool _isOpen = false;   // 실제 상태 — 마스터가 소유
     private Quaternion _closedRot;
     private Quaternion _openRot;
@@ -41,9 +51,16 @@ public class InteractableDoor : MonoBehaviour
     private void RPC_SetDoor(bool open)
     {
         StopAllCoroutines();
-        StartCoroutine(RotateDoor(open ? _openRot : _closedRot));
+        if (doorMode == DoorMode.SlidingDoor)
+        {
+            StartCoroutine(RotateDoor(open ? _openRot : _closedRot));
+        }
+        else if (doorMode == DoorMode.OverheadDoor)
+        {
+            StartCoroutine(OverheadDoor(open ? openY : closeY));
+        }
+        
     }
-
     private IEnumerator RotateDoor(Quaternion target)
     {
         while (Quaternion.Angle(transform.localRotation, target) > 0.5f)
@@ -53,5 +70,27 @@ public class InteractableDoor : MonoBehaviour
             yield return null;
         }
         transform.localRotation = target;
+    }
+    
+    private IEnumerator OverheadDoor(float targetY)
+    {
+        Vector3 target = new Vector3(
+            transform.localPosition.x,
+            targetY,
+            transform.localPosition.z
+        );
+
+        while (Vector3.Distance(transform.localPosition, target) > 0.01f)
+        {
+            transform.localPosition = Vector3.Lerp(
+                transform.localPosition,
+                target,
+                Time.deltaTime * rotateSpeed
+            );
+
+            yield return null;
+        }
+
+        transform.localPosition = target;
     }
 }
